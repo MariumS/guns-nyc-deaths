78440bd00944a78ec0f7b8816783fbcb608330dc
Marium Sultan <mas1300@nyu.edu>
2019-04-25 18:41:46 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,6 @@ var map = new mapboxgl.Map({
   zoom: 10
 });
 
-
-
-
-
-
 map.on('load', function(){
 
   map.addSource('guns', {
@@ -27,17 +22,11 @@ map.on('load', function(){
         type: 'fill',
         source: 'guns',
         paint: {
-          'fill-color': [
-            // use a curve (http://bl.ocks.org/anandthakker/raw/6d0269825a7e0381cdcde13f84a0b6b0/#types-expression-curve)
-            // of type "step," which will step through each break instead of interpolating between them.
-            // Then, get the density value and use a `number` expression to return it as a number instead of a string.
-            // Each step is then a pair [{color code}, {max value for break}]
-            // Finally, add a default color code for any features that fall outside of the steps you've defined.
-            "curve",
-              ["step"], ["number", ["get", "n_killed_t"], 1], "#FFEDA0", 10, "#FED976", 20, "#FEB24C", 50, "#FD8D3C", 100, "#FC4E2A", 200, "#E31A1C", 500, "#BD0026", 2000, "#000000"
-          ],
-          'fill-opacity': 0.6
-        },
-        layout: {}
+          'fill-color': {
+            property: 'n_killed_t',
+            stops: [[1, '#fff'], [40, '#f00']]
+}
+}
       });
       });
+      /*/ http://bl.ocks.org/danswick/d813345baf286a5e0766c6b3d9de01c0 /*/
