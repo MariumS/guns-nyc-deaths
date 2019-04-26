829a343b635b88318dcd0dd33a9f21828f2957ab
Marium Sultan <mas1300@nyu.edu>
2019-04-26 17:23:06 -0400

    beautify

--- js/scripts.js
+++ js/scripts.js
@@ -9,58 +9,58 @@ var map = new mapboxgl.Map({
 
 map.on('load', function() {
 
-      map.addSource('guns', {
-        type: 'geojson',
-        data: './data/map.geojson',
-      });
+  map.addSource('guns', {
+    type: 'geojson',
+    data: './data/map.geojson',
+  });
 
 
-      map.addLayer({
-          id: 'guns_',
-          type: 'fill',
-          source: 'guns',
-          paint: {
-            'fill-color': {
-              property: 'n_killed',
-              stops: [
-                     [0, '#f7cdcd'],
-                     [5, '#ee9f9f'],
-                     [10, '#ea8888'],
-                     [20, '#e15e5e'],
-                     [30, '#dd4a4a'],
-                     [50, '#cc0000'],
-              ]
-            }
-          }
-});
-          // Create a popup, but don't add it to the map yet.
-var popup = new mapboxgl.Popup({
-  closeButton: false,
-  closeOnClick: false
-});
+  map.addLayer({
+    id: 'guns_',
+    type: 'fill',
+    source: 'guns',
+    paint: {
+      'fill-color': {
+        property: 'n_killed',
+        stops: [
+          [0, '#f7cdcd'],
+          [5, '#ee9f9f'],
+          [10, '#ea8888'],
+          [20, '#e15e5e'],
+          [30, '#dd4a4a'],
+          [50, '#cc0000'],
+        ]
+      }
+    }
+  });
+  // Create a popup, but don't add it to the map yet.
+  var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+  });
 
-map.on('mouseenter', 'ZCTA5CE10', function(e) {
-  // Change the cursor style as a UI indicator.
-  map.getCanvas().style.cursor = 'pointer';
+  map.on('mouseenter', 'ZCTA5CE10', function(e) {
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
 
-  var feature = e.features[0];
+    var feature = e.features[0];
 
-  // Populate the popup and set its coordinates
-  // based on the feature found.
-  popup.setLngLat(e.bbox)
-    .setHTML(`
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(e.bbox)
+      .setHTML(`
       <h4>${feature.properties.ZCTA5CE10}</h4><br/>
       <p>Population: ${numeral(feature.properties.n_killed).format('0.0a')}</p>
     `)
-    .addTo(map);
+      .addTo(map);
+  });
+
+  map.on('mouseleave', 'ZCTA5CE10', function() {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
+
+
 });
 
-map.on('mouseleave', 'ZCTA5CE10', function() {
-  map.getCanvas().style.cursor = '';
-  popup.remove();
-});
-
-
-          });
-
-    /*/ source: http://bl.ocks.org/danswick/d813345baf286a5e0766c6b3d9de01c0 /*/
+/*/ source: http://bl.ocks.org/danswick/d813345baf286a5e0766c6b3d9de01c0 /*/
