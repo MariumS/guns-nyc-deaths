6054195c146c0f158fb12142061932316cd90005
Marium Sultan <mas1300@nyu.edu>
2019-04-26 17:18:35 -0400

    block

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,35 @@ map.on('load', function() {
               ]
             }
           }
-      });
+});
+          // Create a popup, but don't add it to the map yet.
+var popup = new mapboxgl.Popup({
+  closeButton: false,
+  closeOnClick: false
+});
+
+map.on('mouseenter', 'ZCTA5CE10', function(e) {
+  // Change the cursor style as a UI indicator.
+  map.getCanvas().style.cursor = 'pointer';
+
+  var feature = e.features[0];
+
+  // Populate the popup and set its coordinates
+  // based on the feature found.
+  popup.setLngLat(e.bbox)
+    .setHTML(`
+      <h4>${feature.properties.ZCTA5CE10}</h4><br/>
+      <p>Population: ${numeral(feature.properties.n_killed).format('0.0a')}</p>
+    `)
+    .addTo(map);
+});
+
+map.on('mouseleave', 'ZCTA5CE10', function() {
+  map.getCanvas().style.cursor = '';
+  popup.remove();
+});
+
+
           });
 
     /*/ source: http://bl.ocks.org/danswick/d813345baf286a5e0766c6b3d9de01c0 /*/
